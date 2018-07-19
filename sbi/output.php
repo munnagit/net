5ab580b1be7b6c0f3d3dd6a6ecdb871f56384a2d
Mohamed Ali <munnayesyou@gmail.com>
2018-07-19 18:37:44 +0530

    Implimented Report Generation

---
+++ sbi/output.php
@@ -0,0 +1,118 @@
+<?php
+include("sessions.php");
+require('mysql_table.php');
+
+// Connect to database
+include("connection.php");
+
+$from= $_POST['from'];
+$to= $_POST['to'];
+
+//echo $from;
+//echo $to;
+
+$pdf=new FPDF();
+
+//Disable automatic page break
+$pdf->SetAutoPageBreak(false);
+
+//Add first page
+$pdf->AddPage();
+
+//set initial y axis position per page
+$y_axis_initial = 25;
+ $y_axis = 18;
+ $row_height = 13;
+
+
+//print column titles
+$pdf->SetFillColor(215,255,215);
+$pdf->SetFont('Arial','B',12);
+$pdf->SetY($y_axis_initial);
+$pdf->SetX(5);
+$pdf->Cell(12,6,'CID',1,0,'L',1);
+$pdf->Cell(12,6,'TID',1,0,'L',1);
+$pdf->Cell(55,6,'NAME',1,0,'C',1);
+$pdf->Cell(14,6,'AMT',1,0,'L',1);
+$pdf->Cell(12,6,'OAP',1,0,'L',1);
+$pdf->Cell(25,6,'OPN',1,0,'C',1);
+$pdf->Cell(20,6,'REFNO',1,0,'L',1);
+$pdf->Cell(25,6,'DATE',1,0,'C',1);
+$pdf->Cell(25,6,'TIME',1,0,'C',1);
+$pdf->SetFont('Arial','',12);
+$pdf->SetFillColor(255,255,255);
+$y_axis = $y_axis + $row_height;
+
+//Select the Products you want to show in your PDF file
+//$result=query('SELECT a.cid,a.name,b.amt FROM tbl_sbitrans b,tbl_sbiclients a where b.cid = a.cid ORDER BY b.stamp desc LIMIT 25',$con);
+$sql="SELECT a.cid,b.tid,a.name,b.amt,b.oap,b.opn,b.refno,b.stamp FROM tbl_sbitrans b,tbl_sbiclients a where b.cid = a.cid and b.stamp > '$from' and b.stamp < '$to' ORDER BY b.stamp desc LIMIT 25";
+//echo $sql;
+$result=$con->query($sql);
+//initialize counter
+$i = 0;
+
+//Set maximum rows per page
+$max = 25;
+
+//Set Row Height
+$row_height = 6;
+
+while($row = $result->fetch_assoc())
+{
+    //If the current row is the last one, create new page and print column title
+    if ($i == $max)
+    {
+        $pdf->AddPage();
+
+        // //print column titles for the current page
+        $pdf->SetY($y_axis_initial);
+        $pdf->SetX(5);
+				$pdf->Cell(30,6,'CID',1,0,'L',1);
+				$pdf->Cell(50,6,'NAME',1,0,'L',1);
+				$pdf->Cell(30,6,'AMOUNT',1,0,'R',1);
+				$pdf->Cell(30,6,'OAP',1,0,'R',1);
+				$pdf->Cell(30,6,'Operation',1,0,'R',1);
+				$pdf->Cell(30,6,'Refno',1,0,'R',1);
+
+        //Go to next row
+        $y_axis = $y_axis + $row_height;
+
+        //Set $i variable to 0 (first row)
+        $i = 0;
+    }
+
+    $cid = $row['cid'];
+		$tid = $row['tid'];
+    $name = $row['name'];
+    $amt = $row['amt'];
+		$oap = $row['oap'];
+		$opn = $row['opn'];
+		$refno = $row['refno'];
+		$stamp = $row['stamp'];
+		$date = date('d-m-Y',strtotime($stamp));
+		$time = date('h:i:s',strtotime($stamp));
+
+    $pdf->SetY($y_axis);
+    $pdf->SetX(5);
+    $pdf->Cell(12,6,$cid,1,0,'L',1);
+		$pdf->Cell(12,6,$tid,1,0,'L',1);
+    $pdf->Cell(55,6,$name,1,0,'C',1);
+    $pdf->Cell(14,6,$amt,1,0,'L',1);
+		$pdf->Cell(12,6,$oap,1,0,'L',1);
+		$pdf->Cell(25,6,$opn,1,0,'L',1);
+		$pdf->Cell(20,6,$refno,1,0,'L',1);
+		$pdf->Cell(25,6,$date,1,0,'L',1);
+		$pdf->Cell(25,6,$time,1,0,'C',1);
+
+    //Go to next row
+    $y_axis = $y_axis + $row_height;
+    $i = $i + 1;
+}
+
+mysqli_close($con);
+
+//Send file
+$pdf->Output();
+
+
+?>
---
+++ sbi/report.php
@@ -0,0 +1,102 @@
+<?php include("sessions.php"); ?>
+<!DOCTYPE html>
+<html>
+
+ <head>
+
+	 <meta charset="utf-8">
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+
+   <link rel="icon" type="image/x-icon" href="images/logo.png" />
+
+
+	 <link rel="stylesheet" href="assets/demo.css">
+	 <link rel="stylesheet" href="assets/form-search.css">
+   <link rel="stylesheet" href="assets/form-basic.css">
+
+   <!-- Required CSS for table -->
+   <!--<link rel="stylesheet" href="assets/normalize.css"> -->
+   <link rel="stylesheet" href="assets/style.css">
+   <script src='http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js'></script>
+
+   <!-- include jQuery library and table sorter plugin http://www.developerdesks.com/simple-table-sorting-in-jquery-with-mysql-data -->
+   <script type='text/javascript' src='js/jquery-latest.js'>
+   </script>
+   <script type='text/javascript' src='js/jquery.tablesorter.min.js'>
+   </script>
+
+   <!-- script needed for sorting table -->
+   <script type='text/javascript'>
+    $(document).ready(function() {
+    $("#table_sort").tablesorter({
+
+    });
+    });
+   </script>
+
+   <!-- Below two scripts are for enabling jquery based date picker to support firefox -->
+   <script type="text/javascript">
+       var datefield = document.createElement("input")
+       datefield.setAttribute("type", "date")
+       if (datefield.type != "date") { //if browser doesn't support input type="date", load files for jQuery UI Date Picker
+           document.write('<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css" />\n')
+           document.write('<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"><\/script>\n')
+           document.write('<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"><\/script>\n')
+       }
+   </script>
+
+   <script>
+       if (datefield.type != "date") { //if browser doesn't support input type="date", initialize date picker widget:
+           jQuery(function ($) { //on document.ready
+               $('#birthday').datepicker();
+           })
+       }
+   </script>
+
+ </head>
+ <body>
+   <?php
+       //phpcode responsibele for displaying tbl_cash row
+       include("headrow.php");
+    ?>
+
+    <ul>
+        <li><a href="index.php">New Client</a></li>
+        <li><a href="./banking/personal.php">Self Banking</a></li>
+        <li><a href="form-search.php" >Search</a></li>
+        <li><a href="trans.php">Transactions</a></li>
+        <li><a href="report.php" class="active">Reports</a></li>
+    </ul>
+
+
+    <div class="main-content">
+      <form  method = "POST" class="form-basic" method="post" action="output.php" target = '_blank'>
+
+          <div class="form-title-row">
+              <h1>Report Generation</h1>
+          </div>
+      <div class="form-row">
+          <label>
+              <span>From</span>
+              <input type="date" id="birthday" name="from" size="20" />
+          </label>
+      </div>
+
+      <div class="form-row">
+          <label>
+              <span>To</span>
+              <input type="date" id="birthday" name="to" size="20" />
+          </label>
+      </div>
+
+      <div class="form-row">
+          <button type="submit" style="margin: 40px 220px 0;">Generate</button>
+      </div>
+
+    </form>
+  </div>
+
+ </body>
+
+</html>
